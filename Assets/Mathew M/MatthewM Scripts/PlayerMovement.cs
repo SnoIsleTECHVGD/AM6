45b6d1f9f0475009f281c737da5917b34d921d0d
Mimikos <1717212@mukilteo.wednet.edu>
2023-12-11 09:32:25 -0800

    Finished character controls

--- Assets/Mathew M/MatthewM Scripts/PlayerMovement.cs
+++ Assets/Mathew M/MatthewM Scripts/PlayerMovement.cs
@@ -7,8 +7,15 @@ public class PlayerMovement : MonoBehaviour
     public KeyCode left;
     public KeyCode right;
     public KeyCode jump;
+    public KeyCode dash;
     public float speed;
     public float jumpAmount = 0.001f;
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashingPower = 24f;
+    public float dashingTime = 0.2f;
+    public float dashingCooldown = 1f;
+
     public Rigidbody2D rb;
     private bool TouchingGrass;
     public LayerMask GroundGaming;
@@ -16,10 +23,14 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
-
+        
     }
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
         if (Input.GetKey(left))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, GetComponent<Rigidbody2D>().velocity.y);
@@ -38,6 +49,30 @@ public class PlayerMovement : MonoBehaviour
             {
                 rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
             }
+        }
+        if (Input.GetKeyDown(dash) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+        IEnumerator Dash()
+        {
+            canDash = false;
+            isDashing = true;
+            float originalGravity = rb.gravityScale;
+            rb.gravityScale = 0f;
+            if (Input.GetKey(left))
+            {
+                rb.velocity = new Vector2(transform.localScale.x * dashingPower * -1, 0f);
+            }
+            else
+            {
+                rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+            }
+            yield return new WaitForSeconds(dashingTime);
+            rb.gravityScale = originalGravity;
+            isDashing = false;
+            yield return new WaitForSeconds(dashingCooldown);
+            canDash = true;
         }
     }
 }
