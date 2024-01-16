e151e256661c5ba4b36c3e39288208b4a97021fa
George <1717784@mukilteo.wednet.edu>
2024-01-16 10:19:50 -0800

    finished end scrip, did some polish

--- Assets/Perry G/End.cs
+++ Assets/Perry G/End.cs
@@ -3,14 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class End : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject gone;
+    public float speedScale = 1f;
+    public Color fadeColor = Color.black;
+    // Rather than Lerp or Slerp, we allow adaptability with a configurable curve
+    public AnimationCurve Curve = new AnimationCurve(new Keyframe(0, 1),
+        new Keyframe(0.5f, 0.5f, -1.5f, -1.5f), new Keyframe(1, 0));
+    public bool startFadedOut = false;
+
+    public GameObject Key;
+    private float alpha = 0f;
+    private Texture2D texture;
+    private int direction = 0;
+    private float time = 0f;
+
+    private void Start()
+    {
+        if (startFadedOut) alpha = 1f; else alpha = 0f;
+        texture = new Texture2D(1, 1);
+        texture.SetPixel(0, 0, new Color(fadeColor.r, fadeColor.g, fadeColor.b, alpha));
+        texture.Apply();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (GetComponent<Key>().hasKey)
+        if (Key.GetComponent<Key>().hasKey)
         {
-            GetComponent<PlayerMovement>().enabled = false;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            player.GetComponent<PlayerMovement>().enabled = false;
+            Destroy(gone);
+            if (direction == 0)
+            {
+                if (alpha >= 1f) // Fully faded out
+                {
+                    alpha = 1f;
+                    time = 0f;
+                    direction = 1;
+                }
+                else // Fully faded in
+                {
+                    alpha = 0f;
+                    time = 1f;
+                    direction = -1;
+                }
+
+            }
+
+            Invoke("LoadCredits",5.0f);
+            Cursor.visible = true;
+        }
+    }
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void OnGUI()
+    {
+        if (alpha > 0f) GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), texture);
+        if (direction != 0)
+        {
+            time += direction * Time.deltaTime * speedScale;
+            alpha = Curve.Evaluate(time);
+            texture.SetPixel(0, 0, new Color(fadeColor.r, fadeColor.g, fadeColor.b, alpha));
+            texture.Apply();
+            if (alpha <= 0f || alpha >= 1f) direction = 0;
         }
     }
 }
