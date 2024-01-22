b7a87952aebe6d9ad7ff1a1521cf4108384b26e3
MUKILTEO\1630410 <1630410@wednet.mukilteo.edu>
2024-01-22 10:15:35 -0800

    thingy fixed

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -23,6 +23,6 @@ public class MainMenu : MonoBehaviour
 
     public void MainScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        SceneManager.LoadScene(1);
     }
 }
