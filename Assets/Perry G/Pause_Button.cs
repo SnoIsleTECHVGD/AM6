eed56160d4356af3fd79b4afd85068fb9eeefae2
George <1717784@mukilteo.wednet.edu>
2024-01-22 09:46:34 -0800

    fixed pause

--- Assets/Perry G/Pause_Button.cs
+++ Assets/Perry G/Pause_Button.cs
@@ -7,6 +7,7 @@ public class Pause_Button : MonoBehaviour
   
     public GameObject player;
     public GameObject PauseScreen;
+    public GameObject key;
   
 
     // Update is called once per frame
@@ -16,13 +17,19 @@ public class Pause_Button : MonoBehaviour
         {
             if(player.GetComponent<PlayerMovement>().enabled == false)
             {
+                player.GetComponent<Rigidbody2D>().gravityScale = 0;
                 player.GetComponent<PlayerMovement>().enabled = true;
                 PauseScreen.SetActive(false);
+                Cursor.visible = true;
+                key.SetActive(false);
             }
             else
             {
                 player.GetComponent<PlayerMovement>().enabled = false;
                 PauseScreen.SetActive(true);
+                player.GetComponent<Rigidbody2D>().gravityScale = 5;
+                Cursor.visible = false;
+                key.SetActive(true);
             }
         }   
         
